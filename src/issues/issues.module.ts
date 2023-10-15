56c62ca713beec7149a3fb1fca76c6221e438be9
maverick369 <maverick0086@gmail.com>
2023-10-15 21:42:11 +0200

    Create http methods in IssuesController

--- src/issues/issues.module.ts
+++ src/issues/issues.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { IssuesController } from './issues.controller';
 import { IssuesService } from './issues.service';
+import { Issue } from './entities/issue.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Issue])],
   controllers: [IssuesController],
   providers: [IssuesService]
 })
