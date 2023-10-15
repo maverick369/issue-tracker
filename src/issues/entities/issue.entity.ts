0bf0432962ccd0bc62063746e052660200769a51
maverick369 <maverick0086@gmail.com>
2023-10-15 20:44:13 +0200

    Create 'Issue' entity

---
+++ src/issues/entities/issue.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity,  PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('issues')
+export class Issue {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  state: number;
+}
