5bdbdeb2bc79cdb54b1461f0a2afeeacb17420b9
maverick369 <maverick0086@gmail.com>
2023-10-15 21:50:42 +0200

    Validate request data with Data Transfer Objects

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // whitelist - remove unnecessary data
+        forbidNonWhitelisted: true, // error message for nonWhiteListes fields
+        transform: true, //  Auto-transform Payloads to DTO instances
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
